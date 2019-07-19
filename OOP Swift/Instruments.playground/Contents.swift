95ca09177cd6b8334d684be76f6859d047b4cfb5
Gteoharov <georgia.teoharov@gmail.com>
2019-07-20 01:16:23 +0300

    OOP Swift

--- OOP Swift/Instruments.playground/Contents.swift
+++ OOP Swift/Instruments.playground/Contents.swift
@@ -1,1 +1,29 @@
 
+class Music {
+    let notes: [String]
+    
+    init(notes: [String]) {
+        self.notes = notes
+    }
+    
+    func prepared() -> String {
+        return notes.joined(separator: " ")
+    }
+}
+
+
+class Instrument {
+    
+    let brand: String
+    
+    init(brand: String) {
+        self.brand = brand
+    }
+    
+    func tune() -> String {
+        fatalError("Implement this for \(brand)")
+    }
+}
+
+let newInstrument = Instrument(brand: "Bosse")
+
