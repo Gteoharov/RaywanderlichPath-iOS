1c2095203649b72d96a32e1f4048f10205ea906f
Gteoharov <georgia.teoharov@gmail.com>
2019-07-20 19:20:21 +0300

    Making a Framework

--- OOP Swift/Instruments.playground/Contents.swift
+++ OOP Swift/Instruments.playground/Contents.swift
@@ -1,39 +1,3 @@
-
-class Music {
-    let notes: [String]
-    
-    init(notes: [String]) {
-        self.notes = notes
-    }
-    
-    func prepared() -> String {
-        return notes.joined(separator: " ")
-    }
-}
-
-
-class Instrument {
-    
-    let brand: String
-    
-    init(brand: String) {
-        self.brand = brand
-    }
-    
-    func tune() -> String {
-        fatalError("Implement this for \(brand)")
-    }
-    
-    func play(_ music: Music) -> String {
-        return music.prepared()
-    }
-    
-    func perform(_ music: Music) {
-        print(tune())
-        print(play(music))
-    }
-}
-
 class Piano: Instrument {
     let hasPedals: Bool
     
@@ -188,6 +152,7 @@ fenderBassGuitar.amplifier.volume
 gibsonElectricGuitar.amplifier.volume
 
 class Band {
+    
     let instruments: [Instrument]
     
     init(instruments: [Instrument]) {
